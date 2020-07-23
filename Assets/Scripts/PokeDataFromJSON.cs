e16e7e9b11003d657bd63720959918250bf59d8c
Thiago Lima <23sarts@gmail.com>
2020-07-23 03:14:45 -0300

    Added New Json Data and Assets

---
+++ Assets/Scripts/PokeDataFromJSON.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using TMPro;
+using UnityEngine.UI;
+
+
+
+public class PokeDataFromJSON : MonoBehaviour
+{
+    public TextAsset pokemonData;
+    public TextAsset DetailsData;
+
+    public List<PokemonData> pokemon;
+
+    public TMP_Text Number, Name, FlavorText;
+    public GameObject Abilities, StatsBars;
+    public Image sprite;
+
+
+
+    public int currentPoke = 0;
+    
+
+    public void GetAllPokemon(){
+        var data = (JObject)JsonConvert.DeserializeObject(pokemonData.text);
+        var data2 = (JObject)JsonConvert.DeserializeObject(DetailsData.text);
+        List<PokeSpecieData> infoData = new List<PokeSpecieData>();
+        for(int i = 0; i < 806; i++){
+            infoData.Add(JsonUtility.FromJson<PokeSpecieData>(data2["pokemon-species"][i].ToString()));
+        }
+        for(int i = 1; i < 807; i++){
+            pokemon.Add(JsonUtility.FromJson<PokemonData>(data["pokemon"][i.ToString()].ToString()));
+            pokemon[i-1].info = infoData[i-1];
+        }
+        infoData.Clear();        
+
+    }
+
+    public void DrawDex(){
+        sprite.gameObject.SetActive(false);
+        PokemonData poke = pokemon[currentPoke];
+        Number.text = poke.id.ToString();
+        Name.text = poke.N;
+        string t = poke.info.FTE[0].e.Replace("\n", "");
+        FlavorText.text = t;
+        sprite.sprite = Resources.Load<Sprite>("Sprites/" + poke.N );
+        sprite.gameObject.SetActive(true);
+    }
+    private void Start()
+    {
+        GetAllPokemon();
+        DrawDex();
+        
+    }
+
+    public void Next(){
+        if(currentPoke < pokemon.Count -1){
+            currentPoke++;
+        }
+        else{
+            currentPoke = 0;
+        }
+        DrawDex();
+    }
+
+    public void Back(){
+        if(currentPoke > 0){
+            currentPoke--;
+        }
+        else{
+            currentPoke = pokemon.Count - 1;
+        }
+        DrawDex();
+    }
+}
---
+++ Assets/Scripts/PokemonData.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+[System.Serializable]
+public class PokemonData
+{
+    public int id;
+    public string N;
+
+    public List<AbilityData> Ab;
+
+    public List<StatsData> St;
+
+    public List<TypeData> T;
+
+    public PokeSpecieData info;
+}
+
+[System.Serializable]
+public class AbilityData
+{
+    public string n;
+    public int id;
+    public bool isH;
+}
+
+[System.Serializable]
+public class StatsData
+{
+    public int bs;
+}
+
+[System.Serializable]
+public class TypeData
+{
+    public string n;
+}
+
+[System.Serializable]
+public class PokeSpecieData{
+    public List<FlavorText> FTE;
+}
+
+[System.Serializable]
+public class FlavorText{
+    public string v;
+    public string e;
+}
